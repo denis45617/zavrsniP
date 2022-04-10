bab35c13038c84c45e422841897cdf042912da7f
Denis Pipalovic <denis.pipalovic@fer.hr>
2022-04-11 01:16:52 +0200

    I dalje je... defaultni taskovi, bez ispisa vremena do prijelaza; nema animacija za +/- bodove,
    Postavke, NAPRAVLJENO NIŠTA. LAME

--- MathSpace/app/src/main/java/com/example/mathspace/MainActivity.java
+++ MathSpace/app/src/main/java/com/example/mathspace/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.mathspace;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.View;
-import android.view.Window;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import androidx.core.view.WindowCompat;
 import com.example.mathspace.hs.HighScore;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,15 +24,7 @@ public class MainActivity extends AppCompatActivity {
         TextView highScoreLabel = findViewById(R.id.highScore);
         highScoreLabel.setText("High score: " + sharedPreferences.getInt("HIGH_SCORE", 0));
 
-
-        this.getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-
+        setFlags();
 
         findViewById(R.id.clearHighScore).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +41,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.settings).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    private void setFlags() {
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 
 
@@ -59,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         updateHighScoreLabel();
-
+        setFlags();
     }
 
     public void updateHighScoreLabel() {
