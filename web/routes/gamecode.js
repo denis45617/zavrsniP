513c5005d4eef12577a4896ac2a9242554d0ab83
Denis Pipalovic <denis.pipalovic@fer.hr>
2022-04-30 20:49:19 +0200

    Adding all types of tasks to some game

--- web/routes/gamecode.js
+++ web/routes/gamecode.js
@@ -7,8 +7,6 @@ const Setting = require("../models/SettingModel");
 router.get('/display/:id', authHandler, async function (req, res, next) {
     let gameCodeSettings;
 
-    //TODO: provjeri da li ovaj id pripada ovom korisniku
-    //i ako pripada onda stavi ovo...
     req.session.game_code = req.params.id;
 
     await (async () => {
@@ -24,49 +22,115 @@ router.get('/display/:id', authHandler, async function (req, res, next) {
     });
 });
 
-router.get('/editsetting/:id', authHandler, async function (req, res, next) {
-    let gameCodeSettings;
 
-    //TODO: provjeri da li ovaj id pripada ovom korisniku
-    //i ako pripada onda stavi ovo...
-    req.session.game_code = req.params.id;
+router.get('/createNewRelativeNumberTask', authHandler, async function (req, res, next) {
+    res.render('relativeNumberTask', {
+        title: 'MathSpace! New relative number task!',
+        settingsId: req.session.game_code,
+        user: req.session.user,
+        linkActive: 'user'
+    });
+
+});
+
+
+router.post('/createNewRelativeNumberTask', authHandler, async function (req, res, next) {
+    let objekt = "";
+    objekt = objekt + "taskObjectType:RelativeNumberTask;";
+    objekt = objekt + "taskText:" + req.body.taskText + ";";
+    objekt = objekt + "taskType:" + req.body.taskType + ";";
+    objekt = objekt + "minNumber:" + req.body.minNumber + ";";
+    objekt = objekt + "maxNumber:" + req.body.maxNumber + ";";
+    objekt = objekt + "relativeNumber:" + req.body.relativeNumber + ";";
+    if (req.body.allowRelativeNumberChange === undefined) {
+        objekt = objekt + "allowRelativeNumberChange:0";
+    } else {
+        objekt = objekt + "allowRelativeNumberChange:1";
+    }
 
     await (async () => {
-        gameCodeSettings = await Setting.getGameCodeSettings(req.params.id);
+        await Setting.createNewTask(req.body.taskText, objekt, req.session.game_code)
     })();
 
-    res.render('Setting', {
-        title: 'User profile',
+    res.redirect("/gamecode/display/" + req.session.game_code)
+});
+
+router.get('/createNewUnrelativeNumberTask', authHandler, async function (req, res, next) {
+    res.render('unrelativeNumberTask', {
+        title: 'MathSpace! New unrelative number task!',
         settingsId: req.session.game_code,
-        settings: gameCodeSettings,
+        user: req.session.user,
+        linkActive: 'user'
+    });
+
+});
+
+router.post('/createNewUnrelativeNumberTask', authHandler, async function (req, res, next) {
+    let objekt = "";
+    objekt = objekt + "taskObjectType:UnrelativeNumberTask;";
+    objekt = objekt + "taskText:" + req.body.taskText + ";";
+    objekt = objekt + "taskType:" + req.body.taskType + ";";
+    objekt = objekt + "minNumber:" + req.body.minNumber + ";";
+    objekt = objekt + "maxNumber:" + req.body.maxNumber;
+
+
+    await (async () => {
+        await Setting.createNewTask(req.body.taskText, objekt, req.session.game_code)
+    })();
+
+    res.redirect("/gamecode/display/" + req.session.game_code)
+});
+
+
+router.get('/createNewShapeTask', authHandler, async function (req, res, next) {
+    res.render('shapeTask', {
+        title: 'MathSpace! New shape number task!',
+        settingsId: req.session.game_code,
         user: req.session.user,
         linkActive: 'user'
     });
 });
 
-router.get('/createnew', authHandler, async function (req, res, next) {
+router.post('/createNewShapeTask', authHandler, async function (req, res, next) {
+    let objekt = "";
+    objekt = objekt + "taskObjectType:ShapeTask;";
+    objekt = objekt + "taskText:" + req.body.taskText + ";";
+    objekt = objekt + "taskType:SHAPE;";
+    objekt = objekt + "askedShape:" + req.body.askedShape;
+
     await (async () => {
-        await Setting.createNewTask("opis", "text", req.session.game_code)
+        await Setting.createNewTask(req.body.taskText, objekt, req.session.game_code)
     })();
 
+    res.redirect("/gamecode/display/" + req.session.game_code)
+});
 
-    res.render('task', {
-        title: 'New task',
+
+router.get('/createNewWordsTask', authHandler, async function (req, res, next) {
+    res.render('wordsTask', {
+        title: 'MathSpace! New words number task!',
         settingsId: req.session.game_code,
         user: req.session.user,
         linkActive: 'user'
     });
 });
 
+router.post('/createNewWordsTask', authHandler, async function (req, res, next) {
+    let objekt = "";
+    objekt = objekt + "taskObjectType:WordsTask;";
+    objekt = objekt + "taskText:" + req.body.taskText + ";";
+    objekt = objekt + "taskType:WORDCONTAINED;";
+    objekt = objekt + "correctWords:" + req.body.correctWords + ";";
+    objekt = objekt + "incorrectWords:" + req.body.correctWords;
 
-router.get('/createnew2', authHandler, async function (req, res, next) {
     await (async () => {
-        await Setting.createNewTask("opis", "text", req.session.game_code)
+        await Setting.createNewTask(req.body.taskText, objekt, req.session.game_code)
     })();
 
-
-    res.redirect('/gamecode/display/'+req.session.game_code);
+    res.redirect("/gamecode/display/" + req.session.game_code)
 });
+
+
 router.get('/remove/:id', authHandler, async function (req, res, next) {
 
     await (async () => {
